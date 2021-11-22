6a3545ccb26cd9fadee8fd4820d92dc1293fbf6a
Alen <aitzhanovalen@gmail.com>
2021-11-22 18:47:23 +0600

    hello world change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello, everyone in World!')
 });
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
